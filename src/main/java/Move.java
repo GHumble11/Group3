ef5076dc37ef7d6780c47d2ef0b7fb4cef46aee4
Ghumble11 <1682975@park.edu>
2022-04-25 16:24:43 -0500

    Changes made on my own over the weekend to Game and Move -George

--- src/main/java/Move.java
+++ src/main/java/Move.java
@@ -1,18 +1,7 @@
-/* List of potential moves the player can take */
-public enum Move {
-    Rock (0, 2),          // move option for throwing rock
-    Paper (1, 0),         // move option for throwing paper
-    Scissors (2, 1);      // move option for throwing scissors
-
-    private final int thisMove;
-    private final int losingMove;
-
-
-    Move(int thisMove, int losingMove) {
-        this.thisMove = thisMove;
-        this.losingMove = losingMove;
-    }
-
-    public int getThisMove() {return this.thisMove;}
-    public int getLosingMove() {return this.losingMove;}
+/** List of potential moves the player can take */
+public enum Move
+{
+    Rock,          // move option for throwing rock, loses to paper
+    Paper,         // move option for throwing paper, loses to scissors
+    Scissors;      // move option for throwing scissors, loses to rock
 }
