854153d261193602e3d4d4d9744b6818a1bb6df5
Ghumble11 <1682975@park.edu>
2022-04-27 21:22:14 -0500

    Changed game logic to if-elses as switch statements didn't work Removed game manager as in theory would not be able to function properly (wouldn't be able to take a new move input for each round) Changed the now only function in Game, play to Game to match the class name 4/27 -George

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -10,7 +10,7 @@ public class Game
      * @param Move2
      * @return gameResult
      */
-    public int play(Move Move1, Move Move2)
+    public int Game(Move Move1, Move Move2)
     {
         int gameResult;
         if(Move1.equals(Move.Rock))
@@ -36,60 +36,5 @@ public class Game
         }
         else { gameResult = 7; }
         return gameResult;
-    }
-
-    /** takes a number of rounds and runs
-     * that many rounds, calling play
-     * every round and taking note of the total
-     * tally for both players
-     * does NOT advance the round counter if a tie
-     * is detected
-     * @param rounds
-     * @return
-     */
-    public String gameManager(int rounds)
-    {
-        //placeholder move values until inputting moves with UI is fully implemented
-        Move p1 = Move.Paper;
-        Move p2 = Move.Rock;
-
-        /**values for our game score
-         *
-         */
-        int s1 = 0;
-        int s2 = 0;
-        for (int i = 0; i < rounds;)
-        {
-            switch (play(p1, p2))
-            {
-                case 1:
-                    System.out.println("Player 1 wins this round");
-                    s1++;
-                    i++;
-                    break;
-                case 2:
-                    System.out.println("Player 2 wins this round");
-                    s2++;
-                    i++;
-                    break;
-                case 3:
-                    System.out.println("Tie, please play again for this rounds score");
-                    break;
-                default:
-                    System.out.println("Error occurred in play, please retry");
-                    break;
-            }
-        }
-
-        if (s1 > s2)
-        {
-            return("Player 1 wins this game with a score of" + s1 + "-" + s2);
-        }
-        else if (s2 > s1)
-        {
-            return("Player 2 wins this game with a score of" + s1 + "-" + s2);
-        }
-        else
-            return("Game results not reached correctly, please try again");
     }
 }
